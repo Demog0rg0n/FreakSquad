43b55aa136cb562344c004a1a05e1051e71a27fd
Demog0rg0n <99020302+Demog0rg0n@users.noreply.github.com>
2022-02-04 17:49:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ if (buster.online == true){
 
 
 
-import TwitchJs from './helix'
+import { ApiClient } from './helix';
 import { StaticAuthProvider } from './twitch-auth';
 
 const clientId = 'k3hjjlrr9t5kofho14epoa4w34skdz';
